d0c9b516a610e683c1e822e01ac8aee28cc117c4
liuminglu19870419 <liuminglu19870419@163.com>
2017-10-10 13:35:16 +0800

    init

---
+++ forward_prop.py
@@ -0,0 +1,17 @@
+'''
+Created on 2017年10月10日
+
+@author: think
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
